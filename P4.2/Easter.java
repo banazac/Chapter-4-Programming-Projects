112fe51626360763314a5331371df30eb79ca6a6
banazac <78494190+banazac@users.noreply.github.com>
2021-02-18 10:03:47 -0600

    Update Easter.java

--- P4.2/Easter.java
+++ P4.2/Easter.java
@@ -1,6 +1,23 @@
 public class Easter
 {
-
+	int y;
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int g;
+	int h;
+	int j;
+	int k;
+	int m;
+	int r;
+	int n;
+	int p;
+	public Easter(int year)
+	{
+		y = year;
+	}
 
    /**
       Constructs the date of Easter Sunday.
@@ -11,11 +28,43 @@ public class Easter
       Gets the month of Easter Sunday
       @return month of Easter Sunday
    */
-
+	public int getEasterSundayMonth()
+	{
+		a = y%19;
+		b = y/100;
+		c = y%100;
+		d = b/4;
+		e = b%4;
+		g = (8*b+13)/25;
+		h = (19*a+b-d-g+15)%30;
+		j = c/4;
+		k = c%4;
+		m = (a+11*h)/319;
+		r = (2*e+2*j-k-h+m+32)%7;
+		n = (h-m+r+90)/25;
+		p = (h-m+r+n+19)%32;
+		return n;
+	}
 
    /**
       Gets the date of Easter Sunday
       @return date of Easter Sunday
    */
-
+	public int getEasterSundayDay()
+	{
+		a = y%19;
+		b = y/100;
+		c = y%100;
+		d = b/4;
+		e = b%4;
+		g = (8*b+13)/25;
+		h = (19*a+b-d-g+15)%30;
+		j = c/4;
+		k = c%4;
+		m = (a+11*h)/319;
+		r = (2*e+2*j-k-h+m+32)%7;
+		n = (h-m+r+90)/25;
+		p = (h-m+r+n+19)%32;
+		return p;
+	}
 }
